c775d39c7023bdb9c0ed59b68a4a856f9479e476
LaVon <leonidmedica@gmail.com>
2022-07-20 10:00:11 +0200

    hw7 is done

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5";
 import HW6 from "../../../p2-homeworks/h6/HW6";
+import HW7 from "../../../p2-homeworks/h7/HW7";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <HW4/>*/}
             <HW5/>
             <HW6/>
+            <HW7/>
 
         </div>
     )
--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -14,10 +14,16 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] =options ? options.map((a,i ) => (
+        <option key={i}
+                value={a}
+        >{a}</option>
+    )):[] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
     return (
